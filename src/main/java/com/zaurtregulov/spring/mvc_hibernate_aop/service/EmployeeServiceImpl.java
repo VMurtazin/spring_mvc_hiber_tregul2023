bc0227061223d181f94ca33b2747bfc76d618d25
VMurtazin <obdorsk@gmail.com>
2023-02-12 22:51:59 +0500

    addGetAll

---
+++ src/main/java/com/zaurtregulov/spring/mvc_hibernate_aop/service/EmployeeServiceImpl.java
@@ -0,0 +1,22 @@
+package com.zaurtregulov.spring.mvc_hibernate_aop.service;
+
+import com.zaurtregulov.spring.mvc_hibernate_aop.dao.EmployeDAO;
+import com.zaurtregulov.spring.mvc_hibernate_aop.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+
+    @Autowired
+    private EmployeDAO employeDAO;
+
+    @Override
+    @Transactional
+    public List<Employee> getAllEmployees() {
+        return employeDAO.getAllEmployees();
+    }
+}
